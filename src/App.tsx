286730aad58e724a08d8f02e4bea84bb8172d816
Edward Nguyen <nhanna@northstudio.vn>
2023-11-28 15:26:08 +0700

    :sparkle: add Framework7 as UI Library

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
-export default function App() {
-  return <>
-    React App
-  </>
+import {App, Link, Navbar, Page, Toolbar, View} from "framework7-react";
+
+export default function () {
+  return <App theme="auto" name="My App">
+
+    {/* Your main view, should have "main" prop */}
+    <View main>
+      {/*  Initial Page */}
+      <Page>
+        {/* Top Navbar */}
+        <Navbar title="Awesome App"></Navbar>
+        {/* Toolbar */}
+        <Toolbar bottom>
+          <Link>Link 1</Link>
+          <Link>Link 2</Link>
+        </Toolbar>
+        {/* Page Content */}
+        <p>Page content goes here</p>
+        <Link href="/about/">About App</Link>
+      </Page>
+    </View>
+  </App>
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import 'framework7/css/bundle';
+import './index.css';
+import Framework7React from "framework7-react";
+import Framework7 from "framework7/bundle";
+
+Framework7.use(Framework7React);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App />
